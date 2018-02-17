2e32c9de1ec9ac303a769539cccd240c5adc0fb5
dwizzel-dev <dwizzel.dev@gmail.com>
2018-02-17 13:54:39 -0500

    fast-route

--- shell/server.sh
+++ shell/server.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 command=$1
 if [ "$command" == "start" ] || [ "$command" == "" ];then
+	echo "stating nginx"
 	sudo systemctl restart nginx.service
+	echo "stating php-fpm"
 	sudo systemctl restart php-fpm.service
-	sudo ps -aux | grep "nginx\|php-fpm"
-	sudo tail -f /var/log/nginx/router-test.dwizzel.local-error.log
+	echo "stating mariadb"
+	sudo systemctl restart mariadb
+	echo "processlist"
+	sudo ps -aux | grep "nginx\|php-fpm\|mariadb"
+	#sudo tail -f /var/log/nginx/composer-test.dwizzel.local-error.log
 elif [ "$command" == "stop" ];then
 	sudo systemctl stop nginx.service
 	sudo systemctl stop php-fpm.service
+	sudo systemctl stop mariadb
 else
 	echo "start|stop"
 fi	
